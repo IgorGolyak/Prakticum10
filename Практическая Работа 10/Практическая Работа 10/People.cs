9a0be1de78e9f90c7627eaacc8e1c79140f11c61
IgorGolyak <golyak.03@inbox.ru>
2021-11-30 13:57:37 +0700

    Prakti10

---
+++ Практическая Работа 10/Практическая Работа 10/People.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Практическая_Работа_10
+{
+    class People
+    {
+        public string FIO;
+        public double ves;
+        public int rost;
+
+        public override string ToString()
+        {
+            return FIO + "  рост=" + rost;
+        }
+    }
+}
